72fd2634a39e081713f163e89b95897785dd163d
Matteo Grilli <matteogrilli2.0@gmail.com>
2023-04-20 22:53:04 +0200

    hand now allows you to inspect a card by hovering over it

--- Decodex App/Assets/Scripts/Decodex/Runtime/Zones/HandController.cs
+++ Decodex App/Assets/Scripts/Decodex/Runtime/Zones/HandController.cs
@@ -11,7 +11,7 @@ using RangeAttribute = UnityEngine.RangeAttribute;
 
 namespace Decodex.Zones
 {
-    public class HandController : ZoneController<LinearCoordinate, CardInstance>, IPointerMoveHandler, IPointerEnterHandler
+    public class HandController : ZoneController<LinearCoordinate, CardInstance>, IPointerMoveHandler, IPointerEnterHandler, IPointerExitHandler
     {
         [BoxGroup("Appearence")]
         [Tooltip("Curvature of the hand. 0 would be a straight line.")]
@@ -37,7 +37,7 @@ namespace Decodex.Zones
         private FanningDirection _fanningDirection;
 
         [BoxGroup("Extend/Retract")]
-        [Label("Enable")]
+        [Label("Enabled")]
         [SerializeField]
         private bool _enableRetract;
 
@@ -47,19 +47,31 @@ namespace Decodex.Zones
         private float _extensionOffset;
 
         [BoxGroup("Inspection")]
+        [Label("Enabled")]
+        [SerializeField]
+        private bool _enableInspection;
+
+        [BoxGroup("Inspection")]
+        [Label("Vertical Offset")]
         [Tooltip("Offset used to have the bottom side of the card align with the bottom of the screen while the card is in close-up")]
         [SerializeField]
-        private float _closeUpOffset;
+        private float _inspectionUpOffset;
 
-        [BoxGroup("Debug")]
-        [Label("Anchors")]
+        [BoxGroup("Inspection")]
+        [Label("Distance from Camera")]
+        [Tooltip("Distance from the camera")]
         [SerializeField]
-        private bool _showAnchors;
+        private float _inspectionDistanceFromCamera = 1f;
 
         [BoxGroup("Debug")]
         [Label("Center of curvature")]
         [SerializeField]
         private bool _showCenterOfCurvature;
+
+        [BoxGroup("Debug")]
+        [Label("Anchors")]
+        [SerializeField]
+        private bool _showAnchors;
 
         [BoxGroup("Debug")]
         [SerializeField]
@@ -72,6 +84,7 @@ namespace Decodex.Zones
         private List<Pose> _anchors;
         private List<GameObject> _slots;
 
+        private int _currentlyInspectedSlotIndex;
         private float _radius;
 
         public override void Init(Zone<LinearCoordinate, CardInstance> model)
@@ -81,6 +94,7 @@ namespace Decodex.Zones
             _centerOfCurvature = CalculateCenterOfCurvature();
             InitAnchors();
             InitSlots();
+            InitInspectedSlot();
             InitPositionController();
             Render();
         }
@@ -94,6 +108,8 @@ namespace Decodex.Zones
             _slots = new();
             CreateSlots();
         }
+
+        private void InitInspectedSlot() => _currentlyInspectedSlotIndex = -1;
 
         [Button]
         public override void Render()
@@ -132,7 +148,7 @@ namespace Decodex.Zones
                 slot.transform.SetParent(transform);
                 slot.transform.localPosition = pose.position;
                 slot.transform.localRotation = pose.rotation;
-                slot.name = $"Slot {i}";
+                slot.GetComponent<HandSlotController>().Init(i);
                 _slots.Add(slot);
             }
         }
@@ -141,12 +157,17 @@ namespace Decodex.Zones
         {
             for (var i = 0; i < zone.ItemsCount(); i++)
             {
-                var slot = _slots[i];
-                var cardInstance = zone.GetAll()[i];
-                var cardInstanceGameObject = GameObject.Find(cardInstance.Id);
-                cardInstanceGameObject.transform.localPosition = slot.transform.position;
-                cardInstanceGameObject.transform.localRotation = slot.transform.rotation;
+                if (i != _currentlyInspectedSlotIndex) ArrangeCardController(i);
             }
+        }
+
+        private void ArrangeCardController(int index)
+        {
+            var slot = _slots[index];
+            var cardInstance = zone.GetAll()[index];
+            var cardInstanceGameObject = GameObject.Find(cardInstance.Id);
+            cardInstanceGameObject.transform.localPosition = slot.transform.position;
+            cardInstanceGameObject.transform.localRotation = slot.transform.rotation;
         }
 
         private GameObject RenderPoseGizmo(string name, Pose pose)
@@ -217,12 +238,55 @@ namespace Decodex.Zones
 
         public void OnPointerMove(PointerEventData eventData)
         {
-            Debug.Log("Moving mouse over hand");
+            if (_enableInspection) UpdateInspectedSlot(eventData);
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            Debug.Log("HAND: Mouse enter");
+            if (_enableInspection) UpdateInspectedSlot(eventData);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (_enableInspection) UpdateInspectedSlot(eventData);
+        }
+
+        private void UpdateInspectedSlot(PointerEventData eventData)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(eventData.position);
+            Physics.Raycast(ray, 1000f, LayerMask.GetMask("Hand Slots"));
+            if (Physics.Raycast(ray, out var hit, 10f, LayerMask.GetMask("Hand Slots")))
+            {
+                InspectSlot(hit.collider.GetComponent<HandSlotController>().Index);
+            } else
+            {
+                _currentlyInspectedSlotIndex = -1;
+            }
+        }
+
+        private void InspectSlot(int index)
+        {
+            if (index == _currentlyInspectedSlotIndex) return;
+
+            // Rearrange previously inspected card
+            if (_currentlyInspectedSlotIndex >= 0)
+                ArrangeCardController(_currentlyInspectedSlotIndex);
+
+            // Calculate inspection position
+            var slot = _slots[index];
+            var slotCenterOnScreen = Camera.main.WorldToScreenPoint(slot.transform.position);
+            Vector3 inspectionPosition = Camera.main.ScreenToWorldPoint(new Vector3(slotCenterOnScreen.x, 0f, _inspectionDistanceFromCamera));
+
+            // Arrange inspected card
+            var cardInstance = zone.GetAll()[index];
+            var cardInstanceGameObject = GameObject.Find(cardInstance.Id);
+            cardInstanceGameObject.transform.position = inspectionPosition;
+            cardInstanceGameObject.transform.position += cardInstanceGameObject.transform.up * _inspectionUpOffset;
+            var upwards = Camera.main.transform.up;
+            var forward = -Camera.main.transform.forward;
+            cardInstanceGameObject.transform.rotation = Quaternion.LookRotation(forward, upwards);
+
+            _currentlyInspectedSlotIndex = index;
         }
     }
 }
--- Decodex App/Assets/Scripts/Decodex/Runtime/Zones/HandSlotController.cs
+++ Decodex App/Assets/Scripts/Decodex/Runtime/Zones/HandSlotController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Graphs;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -7,6 +8,13 @@ namespace Decodex
 {
     public class HandSlotController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
+        public int Index { get; private set; }
+        public void Init(int index)
+        {
+            name = $"Slot {index}";
+            Index = index;
+        }
+
         public void OnPointerEnter(PointerEventData eventData)
         {
             Debug.Log("Slot on enter " + name);
